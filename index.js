91bd859f3c4ac8501d12882b0036c8e9fae931c2
shie1 <sonkolybence@outlook.hu>
2020-11-01 21:26:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -100,6 +100,6 @@ app.listen(port, () => {
         res.json(sCrypt.encrypt(req.query.key, req.query.text));
     });
     app.get("/decrypt", (req, res, next) => {
-        res.json(sCrypt.decrypt(req.query.key, JSON.parse(req.query.array)));
+        res.json(sCrypt.decrypt(req.query.key, req.query.array));
     });
 });
